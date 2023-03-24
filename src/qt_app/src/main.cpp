159eeed15d63525ae8cffce04337bad6170179be
Your Name <you@example.com>
2023-03-24 19:50:33 +0500

    style error

--- src/qt_app/src/main.cpp
+++ src/qt_app/src/main.cpp
@@ -5,7 +5,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+     const QString style_file_path = "home/mortalan/Documents/3D_Viewer-develop_mortalan/src/qt_app/style/style.qss";
     MainWindow w;
+    StyleLoader *style_loader = new StyleLoader(&w, style_file_path);
+    style_loader->attach();
     w.show();
     return a.exec();
 }
